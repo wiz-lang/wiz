a4bef4dbd7a83dda6f4fa0499edd932966b462a3
Andrew G. Crowell <overkill9999@gmail.com>
2019-05-02 01:49:34 -0400

    update version to match last windows build made. (going to try to do more development soon. made a bunch of issues to go through haha)

--- src/wiz/compiler/version.cpp
+++ src/wiz/compiler/version.cpp
@@ -4,12 +4,12 @@
 
 namespace wiz {
     namespace version {
-        extern const char* const Text = "0.1.0 (alpha)";
+        extern const char* const Text = "0.1.1 (alpha; built 2019-05-02)";
 
         // Numeric version format:
         // major (4 digits)
         // minor (2 digits)
         // revision (2 digits).
-        const std::uint32_t ID = UINT64_C(100);
+        const std::uint32_t ID = UINT64_C(101);
     }
 }
